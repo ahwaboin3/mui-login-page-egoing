33e2940b5a662f9870b5c422da8b8defedb2d924
a3 <ahwaboin3@gmail.com>
2025-01-24 10:05:38 +0900

    4hb75634b

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import './App.css';
-import TextField from '@mui/material/TextField';
-import Checkbox from '@mui/material/Checkbox';
-import Button from '@mui/material/Button';
+import { 
+  FormControlLabel, TextField, Checkbox, Button
+ } from '@mui/material';
 
 function App() {
   return (
     <div>
-      <TextField label="Email Address" required fullWidth
-        name="email" autoComplete='email'/>
-      <TextField label="Password" type="password" required
-        fullWidth name="password" autoComplete='current-password'/>
-      <Checkbox value="remember" color="primary" />
-      <Button type="submit">Sign in</Button>
+      <TextField 
+        label="Email Address" 
+        required 
+        fullWidth
+        name="email" 
+        autoComplete='email'
+        autoFocus
+      />
+      <TextField 
+        label="Password" 
+        type="password" 
+        required
+        fullWidth 
+        name="password" 
+        autoComplete='current-password'
+      />
+      <FormControlLabel control={
+          <Checkbox value="remember" color="primary" />
+        }
+        label="Remember me"
+      />
+      <Button 
+        type="submit"
+        fullWidth
+        variant='contained'
+      >Sign in</Button>
     </div>
   )
 }
